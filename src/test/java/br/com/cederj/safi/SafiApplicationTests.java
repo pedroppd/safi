546cd4f8b7408e8b3329af746ef65f4d03368eac
pedro.dantas <pedro.dantas@mobicare.com.br>
2022-03-08 16:52:40 -0300

    project created

---
+++ src/test/java/br/com/cederj/safi/SafiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cederj.safi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
