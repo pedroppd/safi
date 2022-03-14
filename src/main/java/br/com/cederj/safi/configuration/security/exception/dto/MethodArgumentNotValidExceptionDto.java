b02e4cbba28e4e66f96f440b4366c4fc8a7fea87
pedroppd <pedro_pdantas@hotmail.com>
2022-03-13 21:06:51 -0300

    Inserting DTO

---
+++ src/main/java/br/com/cederj/safi/configuration/security/exception/dto/MethodArgumentNotValidExceptionDto.java
@@ -0,0 +1,14 @@
+package br.com.cederj.safi.configuration.security.exception.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MethodArgumentNotValidExceptionDto extends ErrorDto{
+    private String field;
+
+    public MethodArgumentNotValidExceptionDto(int status, String field, String message) {
+        super(status, message);
+        this.setField(field);
+    }
+}
