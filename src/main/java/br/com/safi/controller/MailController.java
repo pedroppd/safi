2b1f83730b90f31125452c1b37a4aed5ed50e724
pedroppd <pedro_pdantas@hotmail.com>
2022-04-30 15:17:24 -0300

    Endpoint para verificar se o usuário está habilitado.

--- src/main/java/br/com/safi/controller/MailController.java
+++ src/main/java/br/com/safi/controller/MailController.java
@@ -1,15 +1,18 @@
 package br.com.safi.controller;
 
+import br.com.safi.models.User;
 import br.com.safi.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/email")
+@Slf4j
 public class MailController {
 
     @Autowired
@@ -21,6 +24,22 @@ public class MailController {
             return ResponseEntity.ok().body("verify_success");
         } else {
             return ResponseEntity.badRequest().body("verify_fail");
+        }
+    }
+
+    @GetMapping("/{userId}/verify")
+    public ResponseEntity<Boolean> userVerify(@PathVariable Long userId) {
+        try {
+            log.debug(String.format("Verifying user with id %n is enable", userId));
+            User user = userService.getUserById(userId);
+            if (user == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok().body(user.isEnabled());
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), "stack", e.getStackTrace());
+            throw e;
         }
     }
 }
